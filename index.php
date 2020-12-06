420e7b4023dd7b40bbb79541d56a3b389a0d3209
MaureenJoy <73148238+MaureenJoy@users.noreply.github.com>
2020-12-06 12:29:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,66 @@
-<?php include_once("index.html"); ?>
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+<meta name="viewport" content="width=device-witdh, initial scale=1">
+<title>Home</title>
+<link rel="stylesheet" type="text/css" href="homestyle.css">
+
+</head>
+<body class="mm">
+    <header >
+    <img class="logo" src="logo.png"   alt="logo">
+    <nav class="navbar">
+     <ul class="nav_links">
+            <li><a href="https://my117mj.herokuapp.com/" style="color: #000000">HOME</a>  </li>
+            <li><a href="product.html"style="color: #000000">PRODUCTS</a>  </li>
+            <li><a href="https://my117about.herokuapp.com/" style="color: #000000">ABOUT</a>  </li>
+            <li><a href="contact.html" style="color: #000000">CONTACT US</a>    </li>
+          </ul>
+</nav>
+      <div class="search">
+       <button type="Submit">Search</button>
+        <input type="text" class="txt"  placeholder="&emsp;&emsp;Search" >
+</div>
+</header>
+<div class="img" >
+    <h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    Welcome to M<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    Customize your own candle
+	<h1>
+<p>
+    
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    Welcome to M, a candle manufacturing in
+    <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    Philippines. We offer handmade candles.
+    <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    The candles are now offered in an array
+    <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    of design, shape, and tetures depend on what
+	 <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	you want.As a ethical manufacturer we want
+    <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    you to experience the maximum enjoyment,
+    <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    pleasure and contentment from our candles.
+    <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    Standardization, competitive price and
+    <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    quality products are our key to sucess.
+    <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    If youare interested to our product,
+	<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	please contact us and we will gladly
+	<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	send you the informations.
+	<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	We are looking to rceive your value inquires.
+</p>
+</div>
+
+
+
+
+</body>
+</html>
